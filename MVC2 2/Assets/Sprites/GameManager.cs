5b4b572e404e2379a05430fd4d91c3fbe36b3add
sashwot <57551518+sashwot@users.noreply.github.com>
2023-02-07 18:45:22 -0600

    Unity Project

---
+++ MVC2 2/Assets/Sprites/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Ball ball;
+    public PlayerI a_player;
+    public PlayerII b_player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
